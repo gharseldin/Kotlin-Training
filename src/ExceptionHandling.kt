fbce7b43636b0e012882759de1f7089524f28526
Amr Gharseldin <agharse@us.ibm.com>
2019-01-04 22:25:22 -0600

    Exception handling
    handling exceptions in Kotlin

---
+++ src/ExceptionHandling.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+
+    var result: Int
+
+    try {
+        result = 9 / 2
+    } catch (e: Exception){
+        result = -1
+    }
+    println("The result = $result")
+}
