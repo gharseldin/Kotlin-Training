a15b750eaa2b6a81c4a144266fc025b4e6106947
Amr Gharseldin <agharse@us.ibm.com>
2019-01-04 22:43:32 -0600

    default parameters
    testing default parameters

--- src/com/gharseldin/section2/Functions.kt
+++ src/com/gharseldin/section2/Functions.kt
@@ -17,6 +17,15 @@ fun main(args: Array<String>){
     else
          "Its false"
     }")
+
+    val n1: Int = 10
+    val n2: Int = 20
+    val n3: Int = 30
+
+    println("Multiplying $n1 & $n2 & $n3 gives ${multiply(n1, n2, n3)} ")
+    println("Multiplying $n1 & $n2 & 10 gives ${multiply(n1, n2)} ")
+
+
 }
 
 fun add(firstNumber: Int, secondNumber:Int): Unit {
@@ -25,4 +34,8 @@ fun add(firstNumber: Int, secondNumber:Int): Unit {
 
 fun addReturn(number1:Int, number2:Int): Int {
     return number1 + number2
+}
+
+fun multiply(num1: Int , num2:Int, num3:Int = 8):Int{
+    return num1 * num2 * num3
 }
