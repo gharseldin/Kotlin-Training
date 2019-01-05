a193037dd8bec235b025216bfed7dbae3ed266ae
Amr Gharseldin <agharse@us.ibm.com>
2019-01-05 13:15:36 -0600

    Constructors
    -played with constructors and initializing blocks

--- src/com/gharseldin/section3/ClassBasics.kt
+++ src/com/gharseldin/section3/ClassBasics.kt
@@ -1,34 +1,28 @@
 package com.gharseldin.section3
 
 fun main(args: Array<String>){
-    var dog1 = Dog()
-    dog1.breed = "Golden Retriever"
-    dog1.size = 2
-    dog1.color = "Golden"
+    var dog1 = Dog("Golden Retriever", 2, "Golden")
 
     dog1.bark()
     dog1.run()
 
-    var dog2 = Dog()
-    dog2.breed = "German Shephard";
-    dog2.size = 3
-    dog2.color = "Black"
+    var dog2 = Dog("German Shephard", 3, "Black")
 
     dog2.bark()
     dog2.run()
 
 }
 
-class Dog {
-    var breed: String = ""
-    var size: Int = 1
-    var color: String = ""
+class Dog (var breed: String, var size:Int, var color:String){
 
+    init {
+        println("Wooh ! Wooh ! I am of size ${this.size}")
+    }
     fun bark(){
-        println("Wooh ! Wooh ! I am of size $size")
+        println("Wooh ! Wooh ! I am of size ${this.size}")
     }
 
     fun run() {
-        println("I am a $breed, my color is $color and I am running!")
+        println("I am a ${this.breed}, my color is ${this.color} and I am running!")
     }
 }
