b57ef6f568938675f91c3db770b969f2c8d10eae
Amr Gharseldin <agharse@us.ibm.com>
2019-01-05 16:09:43 -0600

    Sets in Kotlin
    Sets in Kotlin

---
+++ src/com/gharseldin/section5/Sets.kt
@@ -0,0 +1,21 @@
+package com.gharseldin.section5
+
+fun main(args: Array<String>){
+    val myList = listOf<Int>(2, 2, 3, 4, 5, 6, 6, 6, 10)
+
+    for(x in myList)
+        println(x)
+
+    val mySet = setOf<Int>(2, 2, 3, 4, 5, 6, 6, 6, 10)
+    for(x in mySet)
+        println(x)
+
+    val mutableSet = mutableSetOf<Int>(2, 2, 3, 4, 5, 6, 6, 6, 10)
+    for(x in mutableSet)
+        println(x)
+    mutableSet.add(100)
+    
+    mutableSet.remove(6)
+    for(x in mutableSet)
+        println(x)
+}
