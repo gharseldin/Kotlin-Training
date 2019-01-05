1cde256b392534d58d86559e437fdce7531e9e94
Amr Gharseldin <agharse@us.ibm.com>
2019-01-05 14:42:49 -0600

    higher order functions
    higher order functions

---
+++ src/com/gharseldin/section4/FunctoinalProgramming.kt
@@ -0,0 +1,13 @@
+package com.gharseldin.section4
+
+fun main(args: Array<String>){
+    val mylambdaExpression: (Int, Int)->Int = {x,y -> x+y}
+    addition(10,2, mylambdaExpression)
+
+    val anotherLambda: (Int, Int)-> Unit = { a, b -> println(a+b)}
+    
+}
+
+fun addition(a: Int, b:Int, myFunction:(Int, Int)->Int){
+    println(myFunction(a,b))
+}
