8e961664571d3286de2b8e825bbbd2be39379a3c
Amr Gharseldin <agharse@us.ibm.com>
2019-01-04 22:02:21 -0600

    conditionals and expressions with if
    testing conditionals with and without expressions

---
+++ src/conditionals.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+
+    val number1: Int = 90
+    val number2: Int = 110
+    var greaterNumber:Int
+
+    if (number1 > number2) {
+        println("number1 is greater")
+        greaterNumber = number1
+    } else {
+        println("number2 is greater")
+        greaterNumber = number2
+    }
+    println("The greater number is $greaterNumber")
+
+    // Using an If Expression
+
+    var smallerNumber: Int = if (number1 < number2) {
+                                    println("number1 is smaller")
+                                    number1
+                                } else {
+                                    println("number2 is smaller")
+                                    number2
+                                }
+    println("The smaller number = $smallerNumber")
+}
